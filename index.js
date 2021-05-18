21f9060ce4a1be923df424291a168acf169db1d3
rahiemgoedar <79698303+rahiemgoedar@users.noreply.github.com>
2021-05-18 13:46:02 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,61 @@
 //function declaration
 function squared1(nr1, nr2) {
-    squared1 = ((nr1*nr1+nr2*nr2)*2);
-    return;
+    const squared1 = (nr1*nr1+nr2*nr2);
+    return squared1*squared1;
 }
-squared1(2, 3);
-console.log(squared1);
+
+const result1 = squared1(2, 3);
+console.log(result1);
 
 //function expression
 const squared2 = function (nr1, nr2) {
-return ((nr1*nr1+nr2*nr2)*2);
+    const squared2 = nr1*nr1+nr2*nr2;
+    return squared2*squared2
 };
-squared2(2, 3);
-console.log(squared2);
+
+const result2 = squared2(2, 3);
+console.log(result2);
 
 //arrow functions
-const squared3 = (nr1, nr2) => (nr1*nr1+nr2*nr2*2);
-squared3 (2, 3);
-console.log(squared3);
+const squared3 = (nr1, nr2) => {
+    const sum3 = nr1*nr1+nr2*nr2;
+    return sum3*sum3;
+};
+
+const result3 = squared3(2, 3);
+console.log(result3);
+
+
+/* //hieronder het juiste antwoord:
+function doSquareCalculation(number1, number2) {
+    const number1Squared = number1 * number1;
+    const number2Squared = number2 * number2;
+    const sum = number1Squared + number2Squared;
+    const sumSquared = sum * sum;
+    return sumSquared;
+}
+doSquareCalculation(2,3);
+console.log(doSquareCalculation);
+
+const square = function(number) {
+    return number * number;
+};
+
+const doSquareCalculation = function(number1, number2) {
+    return square(square(number1) + square(number2));
+};
+
+//arrow
+const doSquareCalculation = (number1, number2) => {
+    const number1Squared = number1 * number1;
+    const number2Squared = number2 * number2;
+    const sum = number1Squared + number2Squared;
+    const sumSquared = sum * sum;
+    return sumSquared;
+};
+
+//arrow shorter
+const doSquareCalculation = (number1, number2) => {
+    const sum = number1 * number1 + number2 * number2;
+    return sum * sum;
+}; */
